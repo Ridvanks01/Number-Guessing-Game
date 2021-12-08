0043d15ec66512e0c9c9c4db90bef896fd269f60
Ridvanks01 <ridvan_karasubasi20@erdogan.edu.tr>
2021-12-08 13:19:33 +0300

    Files Sent

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int guess;
+int right = 3;
+Console.WriteLine("** Welcome To Number Guessing Game! **");
+Random rnd = new Random();
+int number = rnd.Next(1, 11);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Make a guess (You have {0} rights):", right);
+    guess = Convert.ToInt32(Console.ReadLine());
+    if (guess == number)
+    {
+        Console.WriteLine("Your guess is true!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Your guess is false!");
+        right--;
+        if (right == 0)
+        {
+            Console.WriteLine("You have no more rights!");
+            break;
+        }
+    }
+}
+Console.WriteLine("Generated number was {0}", number);
