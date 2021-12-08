87577fda5729b5ef04921cf74f290ab9ad1ef9a3
Ridvanks01 <ridvan_karasubasi20@erdogan.edu.tr>
2021-12-08 14:27:50 +0300

    Comments Added

--- Program.cs
+++ Program.cs
@@ -1,26 +1,42 @@
-﻿int guess;
+﻿
+//Integers are defined
+int guess;
 int right = 3;
+//Program name written to console
 Console.WriteLine("** Welcome To Number Guessing Game! **");
+//Random number generated
 Random rnd = new Random();
+//Random number assigned to number value
 int number = rnd.Next(1, 11);
-for (int i = 0; i < 10; i++)
+//Loop started
+for (int i = 0; i < 3; i++)
 {
+    //Our right number has been written to the console
     Console.WriteLine("Make a guess (You have {0} rights):", right);
+    //Guess value received from user
     guess = Convert.ToInt32(Console.ReadLine());
+    //If guess is equal to number it will loop
     if (guess == number)
     {
-        Console.WriteLine("Your guess is true!");
+        // This loop happens if the guess is equal to the number
+        Console.WriteLine("Your guess is correct!");
         break;
     }
+    // This loop happens if the guess is not equal to the number
     else
     {
-        Console.WriteLine("Your guess is false!");
+        //It was written that our guess was wrong
+        Console.WriteLine("Your guess is wrong!");
+        //Our right has been reduced
         right--;
+        //If we run out of right this loop happens
         if (right == 0)
         {
+            //We have written to the console that we have no rights
             Console.WriteLine("You have no more rights!");
             break;
         }
     }
 }
+//We print the randomly generated number to the console
 Console.WriteLine("Generated number was {0}", number);
